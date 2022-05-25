3a78b88a3237e6c1393cb3968397958c917c5f73
poliana.caneves <poli.0808@hotmail.com>
2022-05-24 22:28:19 -0300

    consumindo api

---
+++ app/src/main/java/br/senac/lojaretrofit/services/ProdutoService.kt
@@ -0,0 +1,11 @@
+package br.senac.lojaretrofit.services
+
+import br.senac.lojaretrofit.model.Produto
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ProdutoService {
+
+    @GET("/android/rest/produto")
+    fun listar(): Call<List<Produto>>
+}
